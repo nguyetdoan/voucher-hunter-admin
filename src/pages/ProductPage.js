dfaac9d145ecfe4cd2d167eda7f7eab65d5fe0ef
nguyetdoan <minhnguyetdoan057@gmail.com>
2022-05-15 00:44:06 +0700

    Fix layout product page

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,5 +1,5 @@
-import { Layout } from "antd";
 import React from "react";
+import Layout from "../components/layout";
 import ProductTable from "../components/Products/ProductTable";
 
 const ProductPage = () => {
